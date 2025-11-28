52dff39149be17ef4161d5a440e04489b59374f6
Abhidb2007 <abhishekdb2210@gmail.com>
2025-11-28 14:53:07 +0530

    text-input.tsx

---
+++ packages/ui/src/text-input.tsx
@@ -0,0 +1,14 @@
+interface PropType{
+    placeholder: string;
+}
+export function TextInput({
+    placeholder
+}: PropType){ 
+    return <input style={{
+        padding: 10,
+        margin:10,
+        borderColor:"black",
+        borderWidth:1,
+       
+    }}></input>
+}

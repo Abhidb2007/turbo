a4e0014d278f6bf93d86142f191cbeda007c693e
Abhidb2007 <abhishekdb2210@gmail.com>
2025-11-28 15:19:14 +0530

    add a code

--- packages/ui/src/text-input.tsx
+++ packages/ui/src/text-input.tsx
@@ -1,12 +1,14 @@
 interface PropType{
     placeholder: string;
+    size:"big"|"small"
 }
 export function TextInput({
-    placeholder
+    placeholder,
+    size
 }: PropType){ 
     return <input style={{
-        padding: 10,
-        margin:10,
+        padding:size=="big"? 20:10,
+        margin:size=="big"?20:10,
         borderColor:"black",
         borderWidth:1,
        
